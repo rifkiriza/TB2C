6a6c178a906a49033b957f768df37eb3497bcddc
Gangga Prima Yousya <Gangga@BUYUNG-PC>
2015-05-02 15:15:00 +0700

    hello kawan

--- TB/src/tb/TB.java
+++ TB/src/tb/TB.java
@@ -15,7 +15,7 @@ public class TB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code applicasasation logic here
     }
     
 }
